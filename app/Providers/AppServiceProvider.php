edec993e04afc0da0b4ed332949f2b429f5f963d
kazuki1218 <v.shiotakazuki.1218@gmail.com>
2021-11-14 19:00:57 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,5 +26,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+        if (\App::environment(['production']) || \App::environment(['develop'])) {
+            \URL::forceScheme('https');
+    }
     }
 }
